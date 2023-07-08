e01afa9228daf96acafbf11190cf7616a209b826
seungchan baek <poketsc@gmail.com>
2023-07-08 17:56:31 +0900

    spring kafka를 활용한 consumer application 구현 (메세지 받아서 db 저장)

---
+++ src/main/java/com/example/springkafkaprojectconsumer/SpringKafkaProjectConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkaprojectconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaProjectConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaProjectConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springkafkaprojectconsumer/SpringKafkaProjectConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkaprojectconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaProjectConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
